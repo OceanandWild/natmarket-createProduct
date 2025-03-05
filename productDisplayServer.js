06b2ee4f52e3b18f1e568eb8c0f9119d32672a2d
OceanandWild <149962867+OceanandWild@users.noreply.github.com>
2025-03-05 00:37:10 -0300

    Add files via upload

--- productDisplayServer.js
+++ productDisplayServer.js
@@ -24,3 +24,30 @@ app.get('/products', (req, res) => {
 });
 
 app.listen(3001, () => console.log('Display server running on port 3001'));
+
+const cors = require('cors');
+
+const allowedOrigins = [
+    'https://natmarket.netlify.app',  // Producción
+    'http://127.0.0.1:5502',         // Live Server
+    'http://localhost:3000'          // Desarrollo local
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('CORS not allowed'));
+        }
+    },
+    credentials: true
+}));
+
+// Permitir CORS en todas las respuestas
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');  // Permitir desde cualquier origen (¡Peligroso en producción!)
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+});
